73894b5d97ddf0fb0630a507204eb23a3090b427
bemschaub-uva <bemschaub@uva.nl>
2019-12-08 18:35:37 +0100

    WDID Adding documents to github/my-repo2

---
+++ brt1.R
@@ -0,0 +1,10 @@
+getwd()
+# my repo
+My first ever Github repository
+
+Exercise:
+Write a few lines.
+See what happ in the git tab
+Do the add + commit + to habe your local chages 
+also visible online.
+Hallo ik ben Bart
